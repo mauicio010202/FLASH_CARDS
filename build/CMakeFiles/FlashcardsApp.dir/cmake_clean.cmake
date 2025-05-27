af59a8a9842ae7bdd922ecdaadc2c1779a15fc68
Mauricio Chavez <mauricioch010202@gmail.com>
2025-05-26 20:10:03 -0400

    Added card templates and Makefile

---
+++ build/CMakeFiles/FlashcardsApp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FlashcardsApp.dir/main.cpp.o"
+  "CMakeFiles/FlashcardsApp.dir/src/Card.cpp.o"
+  "FlashcardsApp"
+  "FlashcardsApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FlashcardsApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
